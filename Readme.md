40ad483bdd4ed2131581b83b41aa41e40f8ffc29
Mike Street <mike.street@bozboz.co.uk>
2013-11-25 23:24:19 +0000

    Adding documentation

---
+++ Readme.md
@@ -0,0 +1,72 @@
+#Youtube Viewer
+
+This takes a youtube user and builds up a channel right in the html. It includes users uploads, playlists and an order by selection. Viewing a playlist will change the URL and load that playlist when refreshed/opened in a new window.
+
+To get the basics started, include `jquery` and `youtube.js` into your page.
+
+Make your HTML look like the following:
+
+	<!-- Where your 'sort by' optiosn will appear -->
+	<ul data-role="playlist-order"></ul>
+
+	<!-- Where your playlists will appear -->
+	<ul data-role="playlists"></ul>
+
+	<!-- Where videos will appear -->
+	<ul data-role="videos"></ul>
+
+	<!-- Pagination for the videos -->
+	<a data-role="next-page">Next page</a>
+	<a data-role="prev-page">Prev page</a>
+
+Next, initialise youtube:
+
+	$.youtube();
+
+The youtube plugin takes many, many options - all of them overwriteable. Below are the defaults:
+
+```js
+var defaults = {
+	user: 'mikestreety', // Youtube user
+
+	index_count: 1, // What number video to start from
+	per_page: 12, // How many videos per page
+	page_number: 1, // What page number to start on
+	max_pages: 2, // The maximum pages - this is used by the app
+
+	video_container: 'ul[data-role="videos"]', // Where to put the videos
+	playlist_container: 'ul[data-role="playlists"]', // Where to put the playlists
+	orderby_container: 'ul[data-role="playlist-order"]', // Where to put the filtering
+	next_link: 'a[data-role="next-page"]', // What is the next link
+	prev_link: 'a[data-role="prev-page"]', // What is the prev page link
+
+	// The templates for the vidoes, playlists and filters - enable debug_info to see what values you can have
+	video_list_template: '<li><h3>{title}</h3><h4>{description}</h4><img src="{thumbnail.hqDefault}" alt="{title}"></li>',
+	playlist_template: '<li><a class="{class}" href="#{id}">{title}</a></li>',
+	orderby_template: '<li><a class="{class}" href="#{order}">{title}</a></li>',
+
+	playlist_selected_element: 'a', // What element in the playlist template to apply the selected class to
+	orderby_selected_element: 'a', // What element in the filter template to apply the selected class to
+
+	uploads_title: 'All Uploads', // What link title the uploads playlist has
+	orderby: 'published', // What sorting to use by default
+	selected_class: 'selected', // The class which gets applied to a playlist/filter
+
+	// Array of the filters
+	orderbys: [
+		{title: 'Date', order: 'published'},
+		{title: 'View Count', order: 'viewCount'},
+		{title: 'Title', order: 'title'},
+	],
+
+	// Dev debugging
+	debug_info: false,
+	debug_errors: false,
+
+	// Dev variables - don't really need to be touched
+	current_playlist: window.location.hash.replace(/#/,''),
+	format: 'jsonc',
+	api_version: 2,
+	api_base_url: 'https://gdata.youtube.com/feeds/api'
+}
+```
--- youtube.js
+++ youtube.js
@@ -1,38 +1,43 @@
 (function($){
 	$.youtube = function(options) {
 		var defaults = {
-			index_count: 1,
-			per_page: 2,
-			page_number: 1,
-			max_pages: 2,
+			user: 'mikestreety', // Youtube user
 
-			video_container: 'ul[data-role="videos"]',
-			playlist_container: 'ul[data-role="playlists"]',
-			orderby_container: 'ul[data-role="playlist-order"]',
-			next_link: 'span[data-role="next-page"]',
-			prev_link: 'span[data-role="prev-page"]',
+			index_count: 1, // What number video to start from
+			per_page: 12, // How many videos per page
+			page_number: 1, // What page number to start on
+			max_pages: 2, // The maximum pages - this is used by the app
 
-			playlist_selected_element: 'a',
-			orderby_selected_element: 'a',
+			video_container: 'ul[data-role="videos"]', // Where to put the videos
+			playlist_container: 'ul[data-role="playlists"]', // Where to put the playlists
+			orderby_container: 'ul[data-role="playlist-order"]', // Where to put the filtering
+			next_link: 'a[data-role="next-page"]', // What is the next link
+			prev_link: 'a[data-role="prev-page"]', // What is the prev page link
 
+			// The templates for the vidoes, playlists and filters - enable debug_info to see what values you can have
 			video_list_template: '<li><h3>{title}</h3><h4>{description}</h4><img src="{thumbnail.hqDefault}" alt="{title}"></li>',
 			playlist_template: '<li><a class="{class}" href="#{id}">{title}</a></li>',
 			orderby_template: '<li><a class="{class}" href="#{order}">{title}</a></li>',
 
-			uploads_title: 'All Uploads',
-			orderby: 'published',
-			selected_class: 'selected',
-			user: 'mikestreety',
+			playlist_selected_element: 'a', // What element in the playlist template to apply the selected class to
+			orderby_selected_element: 'a', // What element in the filter template to apply the selected class to
 
+			uploads_title: 'All Uploads', // What link title the uploads playlist has
+			orderby: 'published', // What sorting to use by default
+			selected_class: 'selected', // The class which gets applied to a playlist/filter
+
+			// Array of the filters
 			orderbys: [
 				{title: 'Date', order: 'published'},
 				{title: 'View Count', order: 'viewCount'},
 				{title: 'Title', order: 'title'},
 			],
 
+			// Dev debugging
 			debug_info: false,
 			debug_errors: false,
 
+			// Dev variables - don't really need to be touched
 			current_playlist: window.location.hash.replace(/#/,''),
 			format: 'jsonc',
 			api_version: 2,
@@ -40,12 +45,14 @@
 		}
 		options = $.extend(defaults, options);
 
+		// Set the base urls for the playlists and videos
 		var urls = {
 			user_uploads: options.api_base_url + '/users/' + options.user + '/uploads',
 			user_playlists: options.api_base_url + '/users/' + options.user + '/playlists',
 			playlist_videos: options.api_base_url + '/playlists/'
 		}
 
+		// Globally set the object for the pagination
 		var next_link = $(options.next_link);
 		var prev_link = $(options.prev_link);
 
@@ -59,6 +66,22 @@
 		}
 		// /end templating system
 
+		// Both the playlists and filtering had similar functionality
+		var aside_links = function(self, container, selected_element) {
+			options.index_count = 1;
+			options.page_number = 1;
+
+			self.parents(container).find(selected_element).removeClass(options.selected_class);
+			self.addClass(options.selected_class);
+
+			return self.attr('href').replace(/#/,'');
+		}
+
+		/*
+		*	Videos
+		*/
+
+		// Drawing the video thumbnails
 		var draw_thumbs = function(data) {
 			var container = $(options.video_container);
 			container.fadeOut(500, function(){
@@ -70,25 +93,7 @@
 			})
 		}
 
-		var draw_playlists = function(data) {
-			data.data.items.unshift({
-				id: '',
-				title: options.uploads_title
-			});
-			$.each(data.data.items, function(index, val) {
-				val.class = (val.id == options.current_playlist) ? options.selected_class : '';
-				$(options.playlist_container).append(nano(options.playlist_template, val));
-			});
-		}
-
-		var draw_orderby = function() {
-			var container = $(options.orderby_container)
-			$.each(options.orderbys, function(index, val) {
-				val.class = (val.order == options.orderby) ? options.selected_class : '';
-				container.append(nano(options.orderby_template, val));
-			});
-		}
-
+		// Get the videos - if a playlist URL is provided, use that - if not assume uploads
 		var get_videos = function(url) {
 			url = (url) ?  urls.playlist_videos + url : urls.user_uploads;
 			$.ajax({
@@ -116,6 +121,22 @@
 			});
 		}
 
+		/*
+		*	Playlists
+		*/
+
+		// Draw the playlists using the template & add User Uploads to the beginning of array
+		var draw_playlists = function(data) {
+			data.data.items.unshift({
+				id: '',
+				title: options.uploads_title
+			});
+			$.each(data.data.items, function(index, val) {
+				val.class = (val.id == options.current_playlist) ? options.selected_class : '';
+				$(options.playlist_container).append(nano(options.playlist_template, val));
+			});
+		}
+
 		var get_playlists = function(){
 			$.ajax({
 				url: urls.user_playlists,
@@ -139,10 +160,43 @@
 			});
 		}
 
+		$('body').on('click', options.playlist_container + ' a', function(e) {
+			var link = aside_links($(this), options.playlist_container, options.playlist_selected_element);
+			options.current_playlist = window.location.hash = link;
+			get_videos(link);
+			e.preventDefault();
+		});
+
+		/*
+		*	Filtering
+		*/
+
+		// Draw the filtering options
+		var draw_orderby = function() {
+			var container = $(options.orderby_container)
+			$.each(options.orderbys, function(index, val) {
+				val.class = (val.order == options.orderby) ? options.selected_class : '';
+				container.append(nano(options.orderby_template, val));
+			});
+		}
+
+		// Not really needed - but added for consitency
 		var get_orderbys = function() {
 			draw_orderby();
 		}
 
+		// Filtering link action
+		$('body').on('click', options.orderby_container + ' a', function(e) {
+			options.orderby = aside_links($(this), options.orderby_container, options.orderby_selected_element);
+			get_videos(options.current_playlist);
+			e.preventDefault();
+		});
+
+		/*
+		*	Pagination
+		*/
+
+		// Hides and shows pagination links
 		var pagination_visual = function(data) {
 			options.max_pages = Math.ceil(data.data.totalItems / options.per_page);
 			if(options.page_number === options.max_pages)
@@ -156,6 +210,7 @@
 				prev_link.show();
 		}
 
+		// Keeps track of page and paginates through playlists and uploads
 		var pagination = function(operator) {
 			options.index_count = eval(options.index_count + operator + options.per_page);
 			if(operator == '+')
@@ -166,34 +221,18 @@
 			get_videos(options.playlist_link);
 		}
 
-		var aside_links = function(self, container, selected_element) {
-			options.index_count = 1;
-			options.page_number = 1;
-
-			self.parents(container).find(selected_element).removeClass(options.selected_class);
-			self.addClass(options.selected_class);
-
-			return self.attr('href').replace(/#/,'');
-		}
-
-		$('body').on('click', options.playlist_container + ' a', function(e) {
-			var link = aside_links($(this), options.playlist_container, options.playlist_selected_element);
-			options.current_playlist = window.location.hash = link;
-			get_videos(link);
-			e.preventDefault();
-		});
-		$('body').on('click', options.orderby_container + ' a', function(e) {
-			options.orderby = aside_links($(this), options.orderby_container, options.orderby_selected_element);
-			get_videos(options.current_playlist);
-			e.preventDefault();
-		});
-
 		next_link.on('click', function(){
 			pagination('+');
 		});
 		prev_link.on('click', function(){
 			pagination('-');
 		});
+
+		/*
+		* Initialisation
+		*/
+
+		// Get the three on first load
 		get_videos(options.current_playlist);
 		get_playlists();
 		get_orderbys();
